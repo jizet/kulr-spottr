c8f93afab3bec142cb2812798760d4ebaa6fe0b1
Jose <jose.zuleta@governance.com>
2018-04-19 15:19:12 -0300

    restore files and style

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './App.css';
 import {  BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Provider } from 'react-redux'
 import createStore from './redux'

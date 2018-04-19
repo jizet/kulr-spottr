ccd6c948e2a6340b775beb2dbb67c824515749a0
Jose <jose.zuleta@governance.com>
2018-04-19 15:13:34 -0300

    remove unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './App.css';
 import {  BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Provider } from 'react-redux'
 import createStore from './redux'
